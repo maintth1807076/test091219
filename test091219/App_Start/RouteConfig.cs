dbf41fca5a2d873b38db0a69ad000d41deba125e
maintth1807076 <maintth1807076@fpt.edu.vn>
2019-12-09 17:12:53 +0700

    nop bai

--- test091219/App_Start/RouteConfig.cs
+++ test091219/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace test091219
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Exams", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ test091219/Models/Exam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace test091219.Models
+{
+    public class Exam
+    {
+        public int Id { get; set; }
+        [Display(Name = "Subject")]
+        public string ExamSubject { get; set; }
+        [Display(Name = "Start Time")]
+        public string StartTime { get; set; }
+        [Display(Name = "Exam Date")]
+        public string ExamDate { get; set; }
+        [Display(Name = "Duration")]
+        public int ExamDuration { get; set; }
+        [Display(Name = "Class Room")]
+        public string ClassRoom { get; set; }
+        public string Faculty { get; set; }
+        public StatusType Status { get; set; }
+    }
+    public enum StatusType
+    {
+        Done = 1,
+        Upcoming = 2,
+        Ongoing = 3
+    }
+}
